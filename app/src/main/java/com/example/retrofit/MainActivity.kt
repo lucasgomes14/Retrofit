6f3bf5af283a4be504c53cc13730aba5bac93ef2
lucasgomes14 <lucazmatehus14@gmail.com>
2024-10-18 20:40:51 -0300

    first commit

---
+++ app/src/main/java/com/example/retrofit/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.retrofit
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val service = RetrofitClient.createService(PostService::class.java)
+        val call: Call<List<PostEntity>> = service.list()
+
+        call.enqueue(object : Callback<List<PostEntity>> {
+            override fun onResponse(call: Call<List<PostEntity>>, r: Response<List<PostEntity>>) {
+                val list = r.body()
+            }
+
+            override fun onFailure(call: Call<List<PostEntity>>, t: Throwable) {
+                TODO("Not yet implemented")
+            }
+        })
+    }
+}
